c0c27e1145e910169f47aa83dd03b86ad3467586
Joshua Kemmerer <jdk339@cs.drexel.edu>
2017-06-07 21:51:59 -0400

    WIP for adding actions to new plugin. Added CMB2 to the plugin without attaching it to the plugin

---
+++ wp-content/plugins/my-facebook-tags/my-facebook-tags.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: My Facebook Tags
+ * Plugin URI: http://danielpataki.com
+ * Description: This plugin adds some Facebook Open Graph tags to our single posts.
+ * Version: 1.0.0
+ * Author: Daniel Pataki
+ * Author URI: http://danielpataki.com
+ * License: GPL2
+ */
+
+add_action( 'wp_head', 'my_facebook_tags' );
+
+function my_facebook_tags() {
+  if( is_single() ) {
+  ?>
+    <meta property="og:title" content="<?php the_title() ?>" />
+    <meta property="og:site_name" content="<?php bloginfo( 'name' ) ?>" />
+    <meta property="og:url" content="<?php the_permalink() ?>" />
+    <meta property="og:description" content="<?php the_excerpt() ?>" />
+    <meta property="og:type" content="article" />
+
+    <?php
+      if ( has_post_thumbnail() ) :
+        $image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+    ?>
+      <meta property="og:image" content="<?php echo $image[0]; ?>"/>
+    <?php endif; ?>
+
+  <?php
+  }
+}
